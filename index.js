8c626678c2fd2d7870464b8e7b3bd0c54f6788ad
rachelbarnes <bar13059@byui.edu>
2018-11-15 16:41:10 -0700

    edited app settings

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var app = express();
 const PORT = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.render('pages/index'))
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+.set('views', path.join(__dirname, 'views'))
+.set('view engine', 'ejs')
+.get('/', (req, res) => res.render('pages/index'))
+.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 
 //use res.render to load up an ejs view file
